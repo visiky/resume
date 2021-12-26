79b87f875c4314f197c0ae4c5857536b2a840d45
visiky <736929286@qq.com>
2021-12-26 20:10:43 +0800

    feat: 动态改变网页标题

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import qs from 'query-string';
 import Footer from '@/layout/footer';
 import Content from '@/components';
 import './index.less';
 
 const Page = () => {
+  const [title, changeTitle] = useState('Resume Generator');
+  useEffect(() => {
+    const search = typeof window !== 'undefined' && window.location.search;
+    const query = qs.parse(search);
+    if (query.user) {
+      changeTitle(`${query.user}'s resume`);
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <Helmet>
-        <title>Resume Generator</title>
+        <title>{title}</title>
       </Helmet>
       <Content />
       <Footer />
