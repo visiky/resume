dc38c55cc4979c8ff24f4b79a1f1b7450af5fd88
visiky <736929286@qq.com>
2022-01-05 11:45:37 +0800

    fix: 修复 ssr 下需要判断 document

--- src/locale/language.ts
+++ src/locale/language.ts
@@ -7,6 +7,6 @@ export function getLanguage(): string {
   const search = typeof window !== 'undefined' && window.location.search;
   const query = qs.parse(search);
   const lang = (query.lang as string) || 'zh_CN';
-  document.body.setAttribute('lang', lang);
+  typeof document !== 'undefined' && document.body.setAttribute('lang', lang);
   return lang;
 }
