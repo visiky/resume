0349533c383a2a1ecdb6730d7661e6a2e60a00ff
eric_wang <jadeface.scholar@gmail.com>
2022-03-14 23:44:33 +0800

    fix: only dev support online preview

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { LangSwitcher } from '@/components/LangSwitcher';
-import { useModeSwitcher, Mode } from '@/hooks/useModeSwitcher';
+import { useModeSwitcher } from '@/hooks/useModeSwitcher';
 import { getSearchObj } from '@/helpers/location';
 import env, { Env } from '@/helpers/env';
 import './header.less';
 
 const Header: React.FC = () => {
-  const [ModeSwitcher, mode] = useModeSwitcher({});
+  const [ModeSwitcher] = useModeSwitcher({});
 
   function gotoOnlineVersion() {
     const query = getSearchObj();
@@ -20,7 +20,7 @@ const Header: React.FC = () => {
       <span />
       <span>
         {ModeSwitcher}
-        {env !== Env.Prod && mode !== Mode.Read && (
+        {env !== Env.Prod && (
           <span className={'action-link'} onClick={gotoOnlineVersion}>
             在线版本
           </span>
