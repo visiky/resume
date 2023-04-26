1cea4ce3fec9e4678e51d0e0e3f2d9a8bb448294
visiky <visiky@qq.com>
2023-04-27 00:17:42 +0800

    fix: 修复头像设置问题

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -2,35 +2,25 @@ import React, { useState } from 'react';
 import { Upload, Avatar as AntdAvatar } from 'antd';
 import './index.less';
 
-export const Avatar = ({ avatarSrc, className, shape = 'circle', size = 'default' }) => {
-  const [fileList, setFileList] = useState<any[]>([{ thumbUrl: avatarSrc }]);
-
-  const handleChange = ({ file: newFile, fileList: newFileList }) => {
-    if (newFile.status === 'error') {
-      console.error('upload avatar error:');
-    }
-    setFileList(newFileList);
-  };
-
+export const Avatar = ({
+  avatarSrc,
+  className,
+  shape = 'circle',
+  size = 'default',
+}) => {
   return (
-    <div className={`avatar ${!fileList.length ? 'avatar-hidden' : ''}`}>
-      {fileList.length > 0 && (
+    <div className={`avatar ${!avatarSrc ? 'avatar-hidden' : ''}`}>
+      {avatarSrc ? (
         // @ts-ignore
-        <AntdAvatar className={className} src={fileList[0]?.thumbUrl} shape={shape} size={size} />
+        <AntdAvatar
+          className={className}
+          src={avatarSrc}
+          shape={shape as any}
+          size={size as any}
+        />
+      ) : (
+        <span className="avatar-upload-tip">头像地址为空</span>
       )}
-      <Upload
-        listType="picture-card"
-        fileList={fileList}
-        onChange={handleChange}
-        className={'btn-upload'}
-        style={{ display: fileList.length > 0 ? 'none' : 'block' }}
-      >
-        {fileList.length <= 0 && (
-          <div style={{ color: 'rgba(0, 0, 0, 0.45)' }}>
-            <div className="ant-upload-text">Upload</div>
-          </div>
-        )}
-      </Upload>
     </div>
   );
 };
